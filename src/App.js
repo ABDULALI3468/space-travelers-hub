d18dcb6f11d297fa47f355cc6ae5277f12f18ee7
Manezeu Patricia <pmanezeu@gmail.com>
2022-07-20 14:36:52 +0100

    Listed the missions with styling and reservation functionnality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import Navbar from './components/Navbar';
 import Routes from './routes/routes';
 import store from './Redux/ConfigureStore';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => (
   <Provider store={store}>
