a0b2c186549578654c50099631a59962a29724fb
Manezeu Patricia <pmanezeu@gmail.com>
2022-07-20 15:06:51 +0100

    Added the reserved missions in my profile

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import Table from 'react-bootstrap/Table';
+import '../styles/MyProfile.css';
 
-const MyProfile = () => (
-  <div>This is my profile</div>
-);
+const MyProfile = () => {
+  const reservedMissions = useSelector((state) => (
+    state.missions.filter((mission) => mission.reserved)
+  ));
+
+  return (
+    <div className="profile-content">
+      <section className="reserved-missions">
+        <h1>My Missions</h1>
+        <Table bordered>
+          <tbody>
+            {
+            reservedMissions.map((mission) => (
+              <tr key={mission.mission_id}>
+                <td>{mission.mission_name}</td>
+              </tr>
+            ))
+          }
+          </tbody>
+        </Table>
+      </section>
+
+      <section className="reserved-rockets">
+        <h1>My Rockets</h1>
+        <Table bordered>
+          <tbody>
+            <tr>
+              <td>Mission example</td>
+            </tr>
+          </tbody>
+        </Table>
+      </section>
+    </div>
+  );
+};
 
 export default MyProfile;
