6f2bbed27ab0ae131d47a400b83ca4fde3a441c2
ABDULALI3468 <mrabdulali2003@gmail.com>
2022-07-21 04:13:18 -0700

    Added Redux-logger in package.json and Corrected the styles.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Provider } from 'react-redux';
 import Navbar from './components/Navbar';
 import Routes from './routes/routes';
 import store from './Redux/ConfigureStore';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => (
   <Provider store={store}>
